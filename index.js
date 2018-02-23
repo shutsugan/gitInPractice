40398a923f5029b78f3a110d8d73f49ea2aeaa45
Muhammad Tajjiou <shutsugan@gmail.com>
2018-02-23 15:45:02 +0000

    This might produce a merge conflict.

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const port = 3000;
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Git homepage'));
+app.get('/', (req, res) => res.send('Git homepage so this can make a merge conflict.'));
 
 app.listen(port, _ => console.log(`Git server runing on port ${port}`));
