7c38b3e1d390138e70a5ad829d6c1bc4213c3693
Muhammad Tajjiou <shutsugan@gmail.com>
2018-02-23 12:29:17 +0000

    Add the routes dir.

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 'use strict';
 
 const express = require('express');
+const route = require('./routes/');
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello Git!'));
-app.get('/posts', (req, res) => res.send('Posts page'));
-app.get('/post', (req, res) => res.send('Post page'));
-app.get('/about', (req, res) => res.send('About page'));
-app.get('/contact', (req, res) => res.send('Contact page'));
-
-app.post('/post', (req, res) => res.send('App post page'));
-app.put('/post', (req, res) => res.send('Update post page'));
-app.delete('/post', (req, res) => res.send('Delete post page'));
+route(app);
 
 app.listen(port, _ => console.log(`Git server runing on port ${port}`));
