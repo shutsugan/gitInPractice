af5a8279319962379d9d55904574352880e3b9f7
Muhammad Tajjiou <shutsugan@gmail.com>
2018-02-26 11:19:35 +0000

    2 changs happen on this file.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const express = require('express');
-const route = require('./routes');
 const port = 3000;
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Git homepage so this can make a merge conflict.'));
+app.get('/', (req, res) => res.send('Git home page.'));
+app.get('/git', (req, res) => res.send('The Git sub dir'));
 
 app.listen(port, _ => console.log(`Git server runing on port ${port}`));
