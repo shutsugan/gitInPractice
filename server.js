b23ba7e13dd218a8fde75b4deb1ced840c5f79f9
Muhammad Tajjiou <shutsugan@gmail.com>
2018-02-27 10:28:28 +0000

    Remove git router path from server.js.

--- server.js
+++ server.js
@@ -6,6 +6,5 @@ const port = 3000;
 const app = express();
 
 app.get('/', (req, res) => res.send('Git home page.'));
-app.get('/git', (req, res) => res.send('The Git sub dir'));
 
 app.listen(port, _ => console.log(`Git server runing on port ${port}`));
