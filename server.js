032a179d67fd7e044220ea3a813c99a3ec584d06
Muhammad Tajjiou <shutsugan@gmail.com>
2018-02-28 09:42:55 +0000

    Add some path content.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 const port = 3000;
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.send('Git home page.'));
 
