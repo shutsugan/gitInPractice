3231244ce4fa3d41d64e31cab22c40c30d4837d0
Muhammad Tajjiou <shutsugan@gmail.com>
2018-02-28 09:46:38 +0000

    Add a todo comment and remove the port.

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const express = require('express');
-const port = 3000;
 
 const app = express();
 
 app.get('/', (req, res) => res.send('Git home page.'));
 
-app.listen(port, _ => console.log(`Git server runing on port ${port}`));
+app.listen(3000, _ => console.log(`Git server runing on port 3000`));
+
+//TODO: this is the a comment to trigger the conflict.
