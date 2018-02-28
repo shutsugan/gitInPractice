f45e4fce2ed726a65580f08e4f9990820d5014b7
Muhammad Tajjiou <shutsugan@gmail.com>
2018-02-28 11:42:44 +0000

    Removed and added elements.

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req res) => res.send('Heloo git'));
 
 app.listen(3000, _ => console.log(`Git server runing on port 3000`));
