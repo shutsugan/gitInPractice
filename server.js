ef5d9318ece83e598f025eaa26f06648c27f4a60
Muhammad Tajjiou <shutsugan@gmail.com>
2018-02-28 09:43:49 +0000

    Delete some route content.

--- server.js
+++ server.js
@@ -7,6 +7,4 @@ const port = 3000;
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.send('Git home page.'));
-
 app.listen(port, _ => console.log(`Git server runing on port ${port}`));
