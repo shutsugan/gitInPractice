ddd483a7e69fe9a18e54cb159a894cc0287bc9e1
Muhammad Tajjiou <shutsugan@gmail.com>
2018-02-28 10:01:30 +0000

    Add the path module.

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 'use strict';
 
 const express = require('express');
-<<<<<<< HEAD
-=======
 const path = require('path');
-const port = 3000;
->>>>>>> second-chapter
-
 const app = express();
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, _ => console.log(`Git server runing on port 3000`));
