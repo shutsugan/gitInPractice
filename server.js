bf9d2ee5c5f6c672955a3663d6329c942d739807
Muhammad Tajjiou <shutsugan@gmail.com>
2018-03-05 09:50:07 +0000

    Made some changes.

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => res.send('Heloo git'));
 
 app.listen(3000, _ => console.log(`Git server runing on port 3000`));
 
-//TODO: changed...
+//Some changes to test the pull request.
